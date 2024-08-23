cf95d9c47d30c56c26391132195857303e97f1d7
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-08-23 09:04:53 +0530

    create pages and tailwind layout

---
+++ dashboard/app/(dashboard)/parent/page.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+const ParentPage = () => {
+    return <div>parent page</div>
+}
+export default ParentPage;
---
+++ dashboard/app/components/Menu.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const Menu = () => {
+    return(
+<div className="">Menu</div>
+    )
+}
+
+export default Menu;
