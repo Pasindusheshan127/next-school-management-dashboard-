7a01a7e2f3d1210d6712f848e5d5c94d42ce1706
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-10-02 12:46:40 +0530

    InputField component has been created

---
+++ dashboard/components/InputField.tsx
@@ -0,0 +1,37 @@
+
+import errorsToRecord from '@hookform/resolvers/io-ts/dist/errorsToRecord.js'
+import React from 'react'
+import { FieldError } from 'react-hook-form'
+
+type InputFieldProps = {
+    lable: string,
+    type?: string,
+    register: any,
+    name: string,
+    defaultValue?: string,
+    error?: FieldError,
+    inputProps?:React.InputHTMLAttributes<HTMLInputElement>,
+  
+}
+
+const InputField = (
+    {lable, type = "text", register, name, defaultValue, error, inputProps}:InputFieldProps) =>{
+
+  return (
+    <div className="flex flex-col gap-2 w-full md:w-1/4">
+          <label className="text-xs text-gary-500">Username</label>
+          <input 
+          type="text" {...register(name)} 
+          className="ring-[1.5] ring-gray-300 p-2 rounded-md text-sm border border-black w-full" 
+          {...inputProps}
+          defaultValue={defaultValue}
+          />
+          {error?.message && <p className="text-xs text-red-400">{error?.message.toString()}</p>}
+          <span className="text-xs text-gary-400 font-medium">
+            Personal Information
+          </span>
+      </div>
+  )
+}
+
+export default InputField
--- dashboard/components/forms/TeacherForm.tsx
+++ dashboard/components/forms/TeacherForm.tsx
@@ -4,6 +4,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { log } from "console";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import InputField from "../InputField";
 
 
 const schema = z.object({
@@ -23,13 +24,15 @@ const schema = z.object({
 
 });
 
+type Inputs = z.infer<typeof schema>;
+
 const TeacherForm = ({type,data}:{type:"create" | "update",data:any}) => {
   
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({
+  } = useForm<Inputs>({
     resolver: zodResolver(schema),
   });
 
@@ -40,13 +43,39 @@ const TeacherForm = ({type,data}:{type:"create" | "update",data:any}) => {
 
   return (
     <form action="" className="flex flex-col gap-8" onSubmit={onSubmit}>
-      <h1 className="text-xl font-semibold ">Create a new teacher</h1>
-      <span className="text-xs text-gary-400 font-medium">Authentication Information</span>
-      <label className="text-xs text-gary-500">Username</label>
-      <input type="text" {...register("username")} className="ring-[1.5] ring-gray-300 p-2 rounded-md text-sm border border-black" />
-      {errors.username?.message && <p className="text-xs text-red-400">{errors.username?.message.toString()}</p>}
-      <span className="text-xs text-gary-400 font-medium">Personal Information</span>
-      <button className="bg-blue-400 text-white p-2 rounded-md">{type === "create" ? "Create" : "Update"}</button>
+      <h1 className="text-xl font-semibold ">
+        Create a new teacher
+      </h1>
+
+      <span className="text-xs text-gary-400 font-medium">
+        Authentication Information
+      </span>
+
+      <InputField 
+      lable="username" 
+      name="username" 
+      defaultValue={data?.username} 
+      register={register} 
+      error={errors.username} />
+
+      <InputField 
+      lable="username" 
+      name="username" 
+      defaultValue={data?.username} 
+      register={register} 
+      error={errors.username} />
+      
+      <InputField 
+      lable="username" 
+      name="username" 
+      defaultValue={data?.username} 
+      register={register} 
+      error={errors.username} />
+
+      <button className="bg-blue-400 text-white p-2 rounded-md">
+        {type === "create" ? "Create" : "Update"}
+      </button>
+
     </form>
   )
 }
