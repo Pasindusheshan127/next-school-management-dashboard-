d27932c3bd0a03f0d276dc617bbdb30a51bc8b29
pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-10-02 18:58:53 +0530

    FormModel component has been updated

--- dashboard/components/FormModel.tsx
+++ dashboard/components/FormModel.tsx
@@ -1,9 +1,25 @@
 "use client"
 
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import { useState } from "react";
-import TeacherForm from "./forms/TeacherForm";
-import StudentForm from "./forms/StudentForm";
+// import TeacherForm from "./forms/TeacherForm";
+// import StudentForm from "./forms/StudentForm";
+
+const TeacherForm = dynamic(() => import("./forms/TeacherForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+const StudentForm = dynamic(() => import("./forms/StudentForm"), {
+  loading: () => <h1>Loading...</h1>,
+});
+
+const forms: {
+  [key: string]: (type: "create" | "update", data?: any) => JSX.Element;
+} = {
+  teacher: (type, data) => <TeacherForm type={type} data={data} />,
+  student: (type, data) => <StudentForm type={type} data={data} />
+};
+
 
 const FormModel = ({table,type,data,id}:{
   table:
@@ -36,7 +52,9 @@ const FormModel = ({table,type,data,id}:{
         <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none w-max self-center">Delete</button>
        
       </form>
-    ) : ( <StudentForm type="create" data="" /> )
+    ) : type ==="create" || type ==="update" ?( forms[table](type, data) ) : (
+       "Form not founded"
+    )
   }
 
   return (
